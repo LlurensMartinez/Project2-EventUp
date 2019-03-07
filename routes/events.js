b666159179c643f0bdc62df89a99603b8124bf5b
Tomas Yaya <tomasyaya@Tomass-MacBook-Pro.local>
2019-03-07 17:39:20 +0100

    create event post route working

--- routes/events.js
+++ routes/events.js
@@ -3,13 +3,23 @@
 const express = require('express');
 const router = express.Router();
 const { requireUser } = require('../middlewares/auth');
+const Event = require('../models/Event');
 
 router.get('/new', requireUser, (req, res, next) => {
   res.render('events/create-event');
 });
 
-router.post('new', requireUser, async (req, res, next) => {
-  const { name, username, }
-})
+router.post('/new', requireUser, async (req, res, next) => {
+  const { title, description, address, date, time } = req.body;
+  const event = { title, description, address, date, time };
+  console.log(event);
+  try {
+    event.creator = req.session.currentUser._id;
+    await Event.create(event);
+    res.redirect('/');
+  } catch (error) {
+    next(error);
+  }
+});
 
 module.exports = router;
