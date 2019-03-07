24b602f1953bc26208a6851067e7168b3a568011
Tomas Yaya <tomasyaya@Tomass-MacBook-Pro.local>
2019-03-07 19:03:35 +0100

    add friends working, need more work

--- routes/users.js
+++ routes/users.js
@@ -1,15 +1,28 @@
 const express = require('express');
 const router = express.Router();
 const User = require('../models/User');
+const { requireUser } = require('../middlewares/auth');
 
 /* GET users listing. */
-router.get('/friends', async (req, res, next) => {
+router.get('/friends', requireUser, async (req, res, next) => {
   const { name } = req.query;
   try {
     if (name) {
       var friend = await User.findOne({ name });
     }
     res.render('user/friends', { friend });
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.post('/friends', requireUser, async (req, res, next) => {
+  const { name } = req.body;
+  const { _id } = req.session.currentUser;
+  try {
+    const friend = await User.findOne({ name });
+    await User.findByIdAndUpdate({ _id }, { $push: { friends: friend } });
+    res.redirect('/users/friends');
   } catch (error) {
     next(error);
   }
