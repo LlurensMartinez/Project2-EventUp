ae9e9adb8ef1c718d2198d9e0a6807ad19c98c16
Tomas Yaya <tomasyaya@Tomass-MacBook-Pro.local>
2019-03-12 12:54:37 +0100

    update fb callback url

--- helpers/configuration.js
+++ helpers/configuration.js
@@ -1,5 +1,5 @@
 module.exports = {
   clientID: '816753725352475',
   clientSecret: 'bb43257cb51f337ec9451441569f8cfe',
-  callbackURL: '/auth/facebook/callback'
+  callbackURL: 'https://ironhack-eventup.herokuapp.com/auth/facebook/callback'
 };
