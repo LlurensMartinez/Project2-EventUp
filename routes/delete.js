9b154b741763274806884524251a8746967ae0e0
Tomas Yaya <tomasyaya@Tomass-MacBook-Pro.local>
2019-03-14 12:32:12 +0100

    comments delete working

--- routes/delete.js
+++ routes/delete.js
@@ -3,6 +3,17 @@ const router = express.Router();
 const Event = require('../models/Event');
 const { requireUser } = require('../middlewares/auth');
 
+router.post('/comment/:id', requireUser, async (req, res, next) => {
+  const { id } = req.params;
+  const { _id } = req.session.currentUser;
+
+  try {
+    const event = await Event.findOneAndUpdate({ 'comments._id': id }, { $pull: { comments: { _id: id } } }, { new: true });
+    res.redirect(`/events/info/${event._id}`);
+  } catch (error) {
+    next(error);
+  }
+});
 router.post('/:id', requireUser, async (req, res, next) => {
   const { id } = req.params;
   try {
